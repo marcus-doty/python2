572208802e9757785cf1bcbc57f392963a2c210c
Marcus Doty <115436042+marcus-doty@users.noreply.github.com>
2023-03-13 13:47:29 -0700

    tuple commit

---
+++ hello/notes3.py
@@ -0,0 +1,20 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Tuple ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# A Tuple is a container for a fixed sequence of data objects. Tuples are sequences just like lists. The only difference between them is that tuples cannot be
+# changed -- that is, tuples are immutable. Also, while lists are defined using square brackets, tuples use parentheses. Creating a tuple is as simple as declaring
+# different comma-seperated values. Optionally you can put these values between parentheses.
+
+# Example:
+
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1,2,3,4,5)
+tuple_letters = "a", "b", "c", "d"
+
+# A tuple can be used to group any number of items into a single compound value. Syntactically, a tuple is seperated sequences of values. 
+# Although it is not nessecary, it is conventional to enclose tuples in parentheses.
+
+# Example
+
+dog = ("Canis Familiaris", "dog", "carnivore", 12)
+
+
